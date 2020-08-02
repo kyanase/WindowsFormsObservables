331880825c048bf0d05529098d76f8e54e256a62
kyanase <kengo@yanase.net>
2020-08-02 12:30:53 +0900

    Create EventAsObservableGenerator.

---
+++ WindowsFormsObservablesGenerators.Tests/EventAsObservableGeneratorTest.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsObservablesGenerators.Tests
+{
+    public class EventAsObservableGeneratorTest
+    {
+        [Test]
+        public void Generate()
+        {
+            var clickEvent = typeof(Control).GetEvents().Single(e => e.Name == "Click");
+            var code = EventAsObservableGenerator.GenerateExtensionMethodOfEvent(clickEvent);
+            string expectedCode =
+                @"public static IObservable<EventPattern<EventArgs>> ClickAsObservable(this Control @this)
+    {
+        return Observable.FromEventPattern<EventHandler, EventArgs>(h => @this.Click += h, h => @this.Click -= h);
+    }";
+            Assert.AreEqual(expectedCode, code);
+        }
+    }
+}
---
+++ WindowsFormsObservablesGenerators/EventAsObservableGenerator.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+
+namespace WindowsFormsObservablesGenerators
+{
+    public static class EventAsObservableGenerator
+    {
+        public static string GenerateExtensionMethodOfEvent(EventInfo clickEvent)
+        {
+            var memberInfo = clickEvent.EventHandlerType.GetMethod("Invoke");
+            var parameterInfos = memberInfo.GetParameters();
+            var parameterInfo = parameterInfos[1];
+            var parameterTypeName = parameterInfo.ParameterType.Name;
+            return $@"public static IObservable<EventPattern<{parameterTypeName}>> {clickEvent.Name}AsObservable(this {clickEvent.DeclaringType.Name} @this)
+    {{
+        return Observable.FromEventPattern<{clickEvent.EventHandlerType.Name}, {parameterTypeName}>(h => @this.{clickEvent.Name} += h, h => @this.{clickEvent.Name} -= h);
+    }}";
+        }
+    }
+}
